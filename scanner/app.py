df40f9524d7e79cda5903d5dc6ecf504c27a6453
PatMcN7 <65691549+PatMcN7@users.noreply.github.com>
2022-02-05 16:53:06 -0500

    did the work lol

--- scanner/app.py
+++ scanner/app.py
@@ -3,11 +3,23 @@ import sqlite3
 
 con = sqlite3.connect('data.db')
 cur = con.cursor()
+cur.execute('SELECT time FROM times WHERE name = "RayP"')
+RayP_time_query = cur.fetchall()
+RayP_time = RayP_time_query[0][0]
+cur.execute('SELECT time FROM times WHERE name = "AndrewL"')
+AndrewL_time_query = cur.fetchall()
+AndrewL_time = AndrewL_time_query[0][0]
+cur.execute('SELECT time FROM times WHERE name = "PatrickM"')
+PatrickM_time_query = cur.fetchall()
+PatrickM_time = PatrickM_time_query[0][0]
+cur.execute('SELECT time FROM times WHERE name = "EricJ"')
+EricJ_time_query = cur.fetchall()
+EricJ_time = EricJ_time_query[0][0]
 app = Flask('app')
 
 @app.route('/')
 def index():
-  return render_template('index.html', student_1 = 'RayP', student_2 = 'AndrewL', student_3 = 'PatrickM', student_4 = 'EricJ', student_1_time = '1:00', student_2_time = '2:00', student_3_time = '3:00', student_4_time = '4:00', location ='Classroom')
+  return render_template('index.html', student_1 = 'RayP', student_2 = 'AndrewL', student_3 = 'PatrickM', student_4 = 'EricJ', student_1_time = RayP_time, student_2_time = AndrewL_time , student_3_time = PatrickM_time, student_4_time = EricJ_time, location ='Classroom')
 
 
 
